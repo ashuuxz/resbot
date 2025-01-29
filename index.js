dd9218bcfc3ad9dda0f4689b88c153cf9d23479d
autoresbot <autoresbot@gmail.com>
2025-01-29 22:46:06 +0700

    update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ require('@lib/version');
         const { getAccessToken } = require('@lib/startup');
         await getAccessToken();
     } catch (err) {
-        console.error('Error dalam proses getAccessToken:', err.message);
+        //console.error('Error dalam proses getAccessToken:', err.message);
     }
 })();
 
